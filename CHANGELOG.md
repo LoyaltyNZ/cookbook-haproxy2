d4032dc5aafa17d7c9564eec02e8b0b7cb48de73
Aurynn Shaw <aurynn.shaw@abletech.co.nz>
2015-05-04 11:54:35 +1200

    Update to 0.5.8. Moves 'start haproxy' to after creation of config file.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 0.5.8 (March 24, 2015)
+
+AurynnS: Copied change 5e1cf7d9e268ea64e8676f531f015b6cad8b00b8 from 
+https://github.com/demonccc/chef-repo/, moving "start HAProxy" to happen after
+the config file has been written.
+
 ## 0.5.7 (September 20, 2012)
 
 The initial release.
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "demonccc@gmail.com"
 license           "Apache 2.0"
 description       "Installs and configures haproxy"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.5.7"
+version           "0.5.8"
 
 recipe "haproxy2", "Installs and configures haproxy by searching for nodes of a particular role"
 
